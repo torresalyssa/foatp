8b4c6cf2a056386cabdd566f96028376da73878c
Alyssa Torres <torres.lyssa@gmail.com>
2017-03-01 11:06:23 -0800

    adding vscode settings

--- foatp.py
+++ foatp.py
@@ -3,11 +3,10 @@
 import numpy as np 
 import pandas as pd 
 
-print 'reading in training dataset...'
+print('reading in training dataset...')
 train_df = pd.read_csv('./dataset/train.csv', header=None, sep=',')
 
-print 'reading in testing dataset...'
+print('reading in testing dataset...')
 test_df = pd.read_csv('./dataset/test.csv', header=None, sep=',')
-
 
 
